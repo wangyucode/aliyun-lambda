dc477bad3649875c428f60eba27894480b30adeb
wayne <www_wayne_001@163.com>
2018-10-22 23:31:42 +0800

    以base64编码，需要处理https

--- src/main/java/cn/wycode/lambda/Lambda.java
+++ src/main/java/cn/wycode/lambda/Lambda.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,11 +28,17 @@ public class Lambda implements PojoRequestHandler<Request, Response> {
         logger.info("method->" + request.method);
         logger.info("uri->" + request.uri);
         logger.info("protocolVersion->" + request.protocolVersion);
-        logger.info("uri->" + request.body);
+        logger.info("body->" + request.body);
 
-        URL url = null;
+        URL url;
         try {
-            url = new URI(request.uri).toURL();
+            if (request.uri.startsWith("http://") || request.uri.startsWith("https://") || request.uri.startsWith("HTTP://") || request.uri.startsWith("HTTPS://")) {
+
+            } else {
+                request.uri = "http://" + request.uri;
+            }
+            URI uri = new URI(request.uri);
+            url = uri.toURL();
         } catch (Exception e) {
             response.error = "uri格式错误";
             logger.error(response.error);
@@ -44,6 +51,7 @@ public class Lambda implements PojoRequestHandler<Request, Response> {
             httpUrlConnection.setReadTimeout(30000);
             httpUrlConnection.setRequestMethod(request.method);
             httpUrlConnection.setUseCaches(false);
+            httpUrlConnection.setInstanceFollowRedirects(false);
             for (Map.Entry<String, String> header : request.headers.entrySet()) {
                 httpUrlConnection.setRequestProperty(header.getKey(), header.getValue());
             }
@@ -63,24 +71,33 @@ public class Lambda implements PojoRequestHandler<Request, Response> {
             }
             InputStream httpInputStream = httpUrlConnection.getInputStream();
 
-            String encoding = httpUrlConnection.getContentEncoding();
-            if ("gzip".equalsIgnoreCase(encoding)) {
-                httpInputStream = new GZIPInputStream(httpInputStream);
-                response.headers.remove("Content-Encoding");
-            } else if ("deflate".equalsIgnoreCase(encoding)) {
-                httpInputStream = new DeflaterInputStream(httpInputStream);
-                response.headers.remove("Content-Encoding");
+//            String encoding = httpUrlConnection.getContentEncoding();
+//            if ("gzip".equalsIgnoreCase(encoding)) {
+//                httpInputStream = new GZIPInputStream(httpInputStream);
+//                response.headers.remove("Content-Encoding");
+//            } else if ("deflate".equalsIgnoreCase(encoding)) {
+//                httpInputStream = new DeflaterInputStream(httpInputStream);
+//                response.headers.remove("Content-Encoding");
+//            }
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(httpInputStream, StandardCharsets.UTF_8));
+//            String line = reader.readLine();
+//            while (line!=null) {
+//                sb.append(line);
+//                sb.append('\r');
+//                sb.append('\n');
+//                logger.info(line);
+//                line = reader.readLine();
+//            }
+
+            ByteArrayOutputStream bios = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int n;
+            while ((n = httpInputStream.read(buffer)) != -1) {
+                bios.write(buffer, 0, n);
             }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(httpInputStream, StandardCharsets.UTF_8));
-            String line = reader.readLine();
-            while (line!=null) {
-                sb.append(line);
-                sb.append('\r');
-                sb.append('\n');
-                logger.info(line);
-                line = reader.readLine();
-            }
-            response.content = sb.toString();
+
+            response.content = Base64.getEncoder().encodeToString(bios.toByteArray());
+            bios.close();
             httpInputStream.close();
             httpUrlConnection.disconnect();
         } catch (Exception e) {
